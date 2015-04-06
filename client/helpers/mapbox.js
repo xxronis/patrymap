a3ed338ff922844017cedafc523c13228868cb19
Pan Chronopoulos <xxronis@gmail.com>
2015-04-06 23:49:25 +0300

    always show map, basic style

--- client/helpers/mapbox.js
+++ client/helpers/mapbox.js
@@ -4,7 +4,7 @@ Mapbox.load('markercluster', 'heat');
 Tracker.autorun(function () {
     if (Mapbox.loaded()) {
         L.mapbox.accessToken = 'pk.eyJ1IjoieHhyb25pcyIsImEiOiJscVFVNHdvIn0.H0b4euqdDzJTOz2lpwxe6Q';
-        var map = L.mapbox.map('map', 'xxronis.ll27iaib');
+        var map = L.mapbox.map('map', 'xxronis.ll27iaib');//.setView([-104.99404, 39.75621], 2);
         // Disable drag and zoom handlers.
         //map.dragging.disable();
         map.touchZoom.disable();
@@ -13,5 +13,30 @@ Tracker.autorun(function () {
 
         // Disable tap handler, if present.
         if (map.tap) map.tap.disable();
+
+        function onEachFeature(feature, layer) {
+            // does this feature have a property named popupContent?
+            if (feature.properties && feature.properties.popupContent) {
+                layer.bindPopup(feature.properties.popupContent);
+            }
+        }
+
+        var geojsonFeature = {
+            "type": "Feature",
+            "properties": {
+                "name": "Coors Field",
+                "amenity": "Baseball Stadium",
+                "popupContent": "This is where the Rockies play!"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-104.99404, 39.75621]
+            }
+        };
+
+        L.geoJson(geojsonFeature, {
+            onEachFeature: onEachFeature
+        }).addTo(map);
+        //map.setView([-104.99404, 39.75621], 18);
     }
 });
--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,17 +1,6 @@
 Template.home.rendered = function() {
-    if (Mapbox.loaded()) {
-        L.mapbox.accessToken = 'pk.eyJ1IjoieHhyb25pcyIsImEiOiJscVFVNHdvIn0.H0b4euqdDzJTOz2lpwxe6Q';
-        var map = L.mapbox.map('map', 'xxronis.ll27iaib');
-        // Disable drag and zoom handlers.
-        //map.dragging.disable();
-        map.touchZoom.disable();
-        //map.doubleClickZoom.disable();
-        map.scrollWheelZoom.disable();
 
-        // Disable tap handler, if present.
-        if (map.tap) map.tap.disable();
-    }
 };
 Template.home.helpers({
-    html: '<div id="map"></div>'
+
 });
