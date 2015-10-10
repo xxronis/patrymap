f09c7e07351598055258e7d4edd5941d35255f5f
Pan Chronopoulos <xxronis@gmail.com>
2015-10-10 19:37:33 +0300

    random geo npm integration

--- client/helpers/mapbox.js
+++ client/helpers/mapbox.js
@@ -1,64 +1,72 @@
 Mapbox.debug = true;
 Mapbox.load('markercluster', 'heat');
-
+Meteor.subscribe('geoevents');
 Tracker.autorun(function () {
-    if (Mapbox.loaded()) {
-        L.mapbox.accessToken = 'pk.eyJ1IjoieHhyb25pcyIsImEiOiJscVFVNHdvIn0.H0b4euqdDzJTOz2lpwxe6Q';
-        var map = L.mapbox.map('map', 'xxronis.ll27iaib', { zoomControl: false });//.setView([-104.99404, 39.75621], 2);
-        var geocoder = L.mapbox.geocoder('mapbox.places');
-        geocoder.query('Athens, Greece', showMap);
+  if (Mapbox.loaded()) {
+    L.mapbox.accessToken = 'pk.eyJ1IjoieHhyb25pcyIsImEiOiJscVFVNHdvIn0.H0b4euqdDzJTOz2lpwxe6Q';
+    var map = L.mapbox.map('map', 'xxronis.ll27iaib', { zoomControl: false });//.setView([-104.99404, 39.75621], 2);
+    var geocoder = L.mapbox.geocoder('mapbox.places');
+    geocoder.query('Europe', showEvents);
 
-        var myLayer = L.mapbox.featureLayer().addTo(map);
+    var myLayer = L.mapbox.featureLayer().addTo(map);
 
-        map.touchZoom.disable();
+    map.touchZoom.disable();
 
-        map.scrollWheelZoom.disable();
-        // Disable tap handler, if present.
-        if (map.tap) map.tap.disable();
+    map.scrollWheelZoom.disable();
+    // Disable tap handler, if present.
+    if (map.tap) map.tap.disable();
 
-        function onEachFeature(feature, layer) {
-            // does this feature have a property named popupContent?
-            if (feature.properties && feature.properties.popupContent) {
-                layer.bindPopup(feature.properties.popupContent);
-            }
-        }
-
-        //map.setView([-104.99404, 39.75621], 18);
-        new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
-
-        map.addControl(L.mapbox.geocoderControl('mapbox.places', {
-            keepOpen: true,
-            position: 'bottomleft'
-        }));
-
-
-        function showMap(err, data) {
-            //console.log(data)
-            // The geocoder can return an area, like a city, or a
-            // point, like an address. Here we handle both cases,
-            // by fitting the map bounds to an area or zooming to a point.
-            if (data.lbounds) {
-                map.fitBounds(data.lbounds);
-            } else if (data.latlng) {
-                map.setView([data.latlng[0], data.latlng[1]], 13);
-            }
-            var geojsonFeature = {
-                "type": "Feature",
-                "properties": {
-                    "title": "Athens",
-                    "name": "Athens",
-                    "amenity": "City",
-                    "popupContent": "This is where the shit goes down!"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [data.latlng[1], data.latlng[0]]
-                }
-            };
-
-            L.geoJson(geojsonFeature, {
-                onEachFeature: onEachFeature
-            }).addTo(map);
+    function onEachFeature(feature, layer) {
+        // does this feature have a property named popupContent?
+        if (feature.properties && feature.properties.popupContent) {
+            layer.bindPopup(feature.properties.popupContent);
         }
     }
+
+    function showEvents(err, data) {
+      var markers = [];
+      // The geocoder can return an area, like a city, or a
+      // point, like an address. Here we handle both cases,
+      // by fitting the map bounds to an area or zooming to a point.
+      if (data.lbounds) {
+        map.fitBounds(data.lbounds);
+      } else if (data.latlng) {
+        //map.setView([data.latlng[0], data.latlng[1]], 22);
+      }
+      var geojsonFeature = GeoEvents.find({}).fetch();
+
+      //var athens = {
+      //  "type": "Feature",
+      //    "properties": {
+      //    "title": "Athens",
+      //      "name": "Athens",
+      //      "amenity": "City",
+      //      "popupContent": "This is where the shit goes down!"
+      //  },
+      //    "geometry": {
+      //      "type": "Point",
+      //        "coordinates": [data.latlng[1], data.latlng[0]]
+      //    }
+      //  };
+
+      _.each(geojsonFeature, function(item, index,list) {
+        _.each(item.feature.features, function(marker, index,list){
+          markers.push(marker)
+        });
+      });
+
+      L.geoJson(markers, {
+        onEachFeature: onEachFeature
+      }).addTo(map);
+    }
+
+    //map.setView([-104.99404, 39.75621], 18);
+    new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
+
+
+    map.addControl(L.mapbox.geocoderControl('mapbox.places', {
+        keepOpen: true,
+        position: 'bottomleft'
+    }));
+  }
 });
--- server/seeds/events.js
+++ server/seeds/events.js
@@ -1,19 +1,23 @@
 Meteor.startup(function() {
-
+  var randomGeo = Meteor.npmRequire('geojson-random');
+  //var random = randomGeo.point(1);
   Factory.define('geoevent', GeoEvents, {
     name: function() { return Fake.sentence(); },
     rating: function() { return _.random(1, 5); },
     public: true,
-    owner: 'ywicySTSi7HbxMG2F'
+    owner: 'ywicySTSi7HbxMG2F',
+    feature: function() { return randomGeo.point(1); }
   });
 
-  //if (GeoEvents.find({}).count() === 0) {
+  //console.log(random);
+
+  if (GeoEvents.find({}).count() === 0) {
 
     _(10).times(function(n) {
       Factory.create('geoevent');
     });
 
-  //}
+  }
 
 });
 //Meteor.call(GeoEvents.insert())
